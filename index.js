ad03ba12819ef9683f1661599a672d6a0582f62f
Chase Hoselle <choselle@gmail.com>
2019-04-22 19:42:39 -0500

    working some with middleware

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
+const config = require('config');
+const morgan = require('morgan');
+const helmet = require('helmet');
 const Joi = require('joi'); //Validator package
+const logger = require('./logger');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(helmet());
+
+// Configuration
+console.log('Application Name: ' + config.get('name'));
+console.log('Mail Server: ' + config.get('mail.host'));
+console.log('Mail Password: ' + config.get('mail.password'));
+
+if (app.get('env') === 'development') {
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled...');
+}
+
+app.use(logger);
+
+app.use(function(req, res, next) {
+    console.log('Authenticating...');
+    next();
+});
 
 const courses = [
     { id: 1, name: 'course1' },
     { id: 2, name: 'course2' }
 ]
+
 app.get('/', (req, res) => {
     res.send('Hello World!!!');
 });
